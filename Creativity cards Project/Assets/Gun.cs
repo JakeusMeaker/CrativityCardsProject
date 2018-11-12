1ac8098d03fdfb0b939959d41bd87f6513c41989
Slychaos <jm217495@falmouth.ac.uk>
2018-11-12 21:01:52 +0000

    Redone the walls and redone the Floor +2.5 hours
    Signed-off-by: Slychaos <jm217495@falmouth.ac.uk>

---
+++ Creativity cards Project/Assets/Gun.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public float dmg = 10f;
+    public float range = 100;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(dmg);
+            }
+
+            GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impact, 2f);
+        }
+    }
+}
---
+++ Creativity cards Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public float restartDelay = 1f;
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void EndGame()
+    {
+        Restart();
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
